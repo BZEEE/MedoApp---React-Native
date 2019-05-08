4d50a674a7377247b42fd5dc245d88cc6e3bb396
bjzuk <bjzukows@ualberta.ca>
2019-05-08 18:28:44 -0400

    (WIP) added canvas to ViewScan screen

--- src/components/screens/ViewScanScreen/ViewScan.android.js
+++ src/components/screens/ViewScanScreen/ViewScan.android.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react"
 import { View, Text } from "react-native"
+import Canvas from "react-native-canvas"
 const styles = require("./ViewScanStyleSheet")
 
 class ViewScanScreen extends React.Component {
 
     static navigationOptions = {}
 
+    handleCanvas = (canvas) => {
+        const context = canvas.getContext('2d');
+        context.fillStyle = "purple";
+        context.fillRect(0, 0, 100, 100);
+    }
+
     render() {
         return(
-            <View style={styles.viewStyle}>
-                <Text style={styles.welcome}>This is where you log in</Text>
+            <View>
+                <Canvas ref={this.handleCanvas}/>
             </View>
         )
     }
